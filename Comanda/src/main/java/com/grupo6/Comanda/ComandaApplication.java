671ccc747ca7bb4ff2fbaf66b6f61885ad39b712
DiegoOcsaCaceres <diego08072005@gmail.com>
2026-05-14 10:26:08 -0500

    Create backend project

---
+++ Comanda/src/main/java/com/grupo6/Comanda/ComandaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.Comanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandaApplication.class, args);
+	}
+
+}
---
+++ Comanda/src/test/java/com/grupo6/Comanda/ComandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.Comanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
