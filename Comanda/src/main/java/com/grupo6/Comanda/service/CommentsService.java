3e0a61af0091a183003b85d00fef14e491520e49
DiegoOcsaCaceres <diego08072005@gmail.com>
2026-05-16 18:27:29 -0500

    Add CommentsService.java

---
+++ Comanda/src/main/java/com/grupo6/Comanda/service/CommentsService.java
@@ -0,0 +1,20 @@
+package com.grupo6.Comanda.service;
+
+import com.grupo6.Comanda.model.entities.CommentEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CommentsService {
+
+    List<CommentEntity> list(Long restaurantId, String tipo);
+
+    CommentEntity submit(CommentEntity comment);
+
+    ResponseEntity<Map<String, Object>> markRead(Long id);
+
+    ResponseEntity<Void> delete(Long id);
+
+    Map<String, Long> unreadCount();
+}
