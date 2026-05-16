2920d56cea485a8fa04d184a0d327556888d5135
DiegoOcsaCaceres <diego08072005@gmail.com>
2026-05-16 16:51:24 -0500

    Add UserRepository

---
+++ Comanda/src/main/java/com/grupo6/Comanda/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.grupo6.Comanda.repository;
+
+import com.grupo6.Comanda.model.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
+
