8e61bc3c0e4426b8ab27820fddede96a4245c417
DiegoOcsaCaceres <diego08072005@gmail.com>
2026-05-16 16:55:35 -0500

    Add Reservation and Comment

---
+++ Comanda/src/main/java/com/grupo6/Comanda/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.grupo6.Comanda.repository;
+
+import com.grupo6.Comanda.model.entities.CommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findByRestaurant_Id(Long restaurantId);
+    List<CommentEntity> findByRestaurant_IdAndTipo(Long restaurantId, String tipo);
+    List<CommentEntity> findByTipo(String tipo);
+    Long countByLeidoFalse();
+}
