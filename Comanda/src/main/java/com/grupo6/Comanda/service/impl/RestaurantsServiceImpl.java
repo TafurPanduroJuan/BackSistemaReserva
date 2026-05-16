40f252d4c4d3948bd41ca43afd44332c99b9cd2b
DiegoOcsaCaceres <diego08072005@gmail.com>
2026-05-16 18:54:03 -0500

    Add list RestaurantsServiceImpl

---
+++ Comanda/src/main/java/com/grupo6/Comanda/service/impl/RestaurantsServiceImpl.java
@@ -0,0 +1,77 @@
+package com.grupo6.Comanda.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+
+import com.grupo6.Comanda.model.entities.RestaurantEntity;
+import com.grupo6.Comanda.model.entities.RestaurantRequestEntity;
+import com.grupo6.Comanda.repository.RestaurantRepository;
+import com.grupo6.Comanda.repository.RestaurantRequestRepository;
+import com.grupo6.Comanda.service.RestaurantsService;
+
+public class RestaurantsServiceImpl implements RestaurantsService{
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantRequestRepository requestRepository;
+
+    public RestaurantsServiceImpl(RestaurantRepository restaurantRepository,
+                                  RestaurantRequestRepository requestRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.requestRepository    = requestRepository;
+    }
+
+    @Override
+    public List<RestaurantEntity> listAll() {
+        return restaurantRepository.findAll();
+    }
+
+    @Override
+    public ResponseEntity<RestaurantEntity> getOne(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOne'");
+    }
+
+    @Override
+    public RestaurantEntity create(RestaurantEntity restaurant) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public ResponseEntity<RestaurantEntity> update(Long id, RestaurantEntity updated) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public List<RestaurantRequestEntity> listRequests(String estado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listRequests'");
+    }
+
+    @Override
+    public RestaurantRequestEntity submitRequest(RestaurantRequestEntity req) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'submitRequest'");
+    }
+
+    @Override
+    public ResponseEntity<Map<String, Object>> acceptRequest(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'acceptRequest'");
+    }
+
+    @Override
+    public ResponseEntity<Map<String, Object>> rejectRequest(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rejectRequest'");
+    }
+    
+}
