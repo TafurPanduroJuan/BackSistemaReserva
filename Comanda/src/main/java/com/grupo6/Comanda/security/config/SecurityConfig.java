d6d89f0703b331714d5312325cc5db4f1117ffb8
HilarioCcuchoJuanPablo <juanhilario2001@gmail.com>
2026-05-24 23:17:08 -0500

    configure AuthenticationManager in SecurityConfig

--- Comanda/src/main/java/com/grupo6/Comanda/security/config/SecurityConfig.java
+++ Comanda/src/main/java/com/grupo6/Comanda/security/config/SecurityConfig.java
@@ -111,4 +111,11 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
+
+
 
