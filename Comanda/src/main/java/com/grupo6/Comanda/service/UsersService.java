a63efa58bb585dbc0b87f32a30f8957c821f7d1a
DiegoOcsaCaceres <diego08072005@gmail.com>
2026-06-01 11:44:48 -0500

    update UserService

--- Comanda/src/main/java/com/grupo6/Comanda/service/UsersService.java
+++ Comanda/src/main/java/com/grupo6/Comanda/service/UsersService.java
@@ -1,20 +1,25 @@
 package com.grupo6.Comanda.service;
 
-import java.util.List;
-import java.util.Map;
+import com.grupo6.Comanda.auth.dto.AuthDtos.UpdateMeRequest;
+import com.grupo6.Comanda.model.entities.UserEntity;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.grupo6.Comanda.model.entities.UserEntity;
+import java.util.List;
+import java.util.Map;
 
 public interface UsersService {
-    
+
     List<UserEntity> listAll();
 
     ResponseEntity<UserEntity> getOne(Long id);
+
     ResponseEntity<Map<String, Object>> changeRole(Long id, Map<String, String> body);
+
     ResponseEntity<Void> delete(Long id);
+
     ResponseEntity<UserEntity> getMe(UserDetails principal);
-    ResponseEntity<UserEntity> updateMe(UserDetails principal, Map<String, String> body);
+
+    ResponseEntity<UserEntity> updateMe(UserDetails principal, UpdateMeRequest body);
 }
